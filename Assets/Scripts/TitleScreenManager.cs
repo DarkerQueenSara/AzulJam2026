a35317f3dbd9c4a4f485fe632fe87d9cdc58afa5
Diogo Tito Victor Marques <diogotitomarques@gmail.com>
2026-03-21 17:33:31 +0000

    Write basic "Press any button" logic on TitleScreen

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -1,16 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using BuzzControllerSystem;
 
 public class TitleScreenManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    const int NUM_BUZZ_PLAYERS = 4;
+    const int NUM_BUZZ_BUTTONS = 5;
+
+    public BuzzInput buzzInput;
+
     void Start()
     {
-        
+        buzzInput = GetComponent<BuzzInput>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        for (int player = 0; player < NUM_BUZZ_PLAYERS; player++)
+        {
+            for (int button = 0; button < NUM_BUZZ_BUTTONS; button++)
+            {
+                var buzzButton = (BuzzInput.BuzzButton)button;
+                if (buzzInput.GetButtonDown(player, buzzButton))
+                {
+                    //Load the next scene
+                    UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+                }
+            }
+        }
     }
 }
