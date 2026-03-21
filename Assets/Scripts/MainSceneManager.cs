9b6607adce8930e7aae80646fc0da3cbab7cb0a2
Sara Marques <saramarquesgamedev@gmail.com>
2026-03-21 17:31:09 +0000

    Further progress on tutorial and main scene

--- Assets/Scripts/MainSceneManager.cs
+++ Assets/Scripts/MainSceneManager.cs
@@ -1,12 +1,42 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainSceneManager : MonoBehaviour
 {
+
+    //there will be a list here with questions, but they should be
+    //ScriptableObjects so the logic comes with them
+    public string discussionString;
+    public string betTargetString;
+    public string betTypeString;
+    
+    public TextMeshProUGUI commandText;
+    public GameObject redButtonHolder;
+    
+    private List<Image> _redButtons = new List<Image>();
+    
+    private int[] _scores;
+    //Two numbers: the player that voted, and who they voted for
+    //they come in at different orders
+    private List<KeyValuePair<int, int>> _votes;
+    private bool[] _deadPlayers;
+    private bool[] _readyPlayers;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        _scores = new int[4];
+        _votes = new List<KeyValuePair<int, int>>();
+        _deadPlayers = new bool[4];
+        _readyPlayers = new bool[4];
+
+        for (int i = 0; i < 4; i++)
+        {
+            _scores[i] = 0;
+            _redButtons.Add(redButtonHolder.transform.GetChild(i).GetComponent<Image>());
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/TutorialScreenManager.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TutorialScreenManager : MonoBehaviour
+{
+
+    public List<string> tutorialTextLines;
+
+    public List<string> tutorialText;
+
+    private int _currentIndex = 0;
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        //check for button presses
+        //advance to next string
+    }
+}
