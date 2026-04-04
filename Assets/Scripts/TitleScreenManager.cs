ab91902d533fd5ffc6635c37f845cce82d88eea7
diogotito <diogotitomarques@gmail.com>
2026-04-04 17:14:50 +0100

    Recreate Buzz light mode move

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -1,5 +1,8 @@
+using System.Collections;
+using System.Linq;
 using UnityEngine;
 using BuzzControllerSystem;
+using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.SceneManagement;
 using static WitchGameplay;
 
@@ -8,10 +11,24 @@ public class TitleScreenManager : MonoBehaviour
 {
     private async Awaitable Start()
     {
-        // TODO restore hype light sequence
-        // buzzInput.StartLightSequence(BuzzInput.BuzzLightSequence.FastMove);
-        
+        StartCoroutine(BuzzHypeCoroutine());
         await GetComponent<WitchInput>().onConfirmation;
+        StopAllCoroutines();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private static readonly YieldInstruction Wait = new WaitForSeconds(0.25f);
+    
+    private static IEnumerator BuzzHypeCoroutine()
+    {
+        var device = BuzzInputDevice.current;
+        if (device == null) yield break;
+
+        for (var p = 0; /* forever */; p = (p + 1) % PlayerCount)
+        {
+            var lights = BuzzOutputReport.Create(p == 0, p == 1, p == 2, p == 3);
+            device.ExecuteCommand(ref lights);
+            yield return Wait;
+        }
     }
 }
